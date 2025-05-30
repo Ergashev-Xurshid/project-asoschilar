ff069657df0fdc321fc99b56811dd81718e8884b
Ergashev-Xurshid <xurshidgithub@gmail.com>
2025-05-30 22:39:54 +0500

    add section

---
+++ js/app.js
@@ -0,0 +1,8 @@
+ var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      navigation: {
+        nextEl: ".swiper-button-next1",
+        prevEl: ".swiper-button-prev1",
+      },
+    });
